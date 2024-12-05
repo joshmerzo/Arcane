c9ce7c183a21ee607fbccd3f5b0090cbbe5f947d
joshmerzo <jozrem183@gmail.com>
2024-12-05 12:57:30 +0800

    edited media queries for hamburger

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const menu = document.querySelector('.navbar-menu');
 toggler.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    document.querySelector('.navbar-menu').classList.toggle('show');
+});
